7b91f544168ba7e6792d6ad0719ed72e0a7dd13c
Alireza Lavasani <alireza.sh.lavasani@gmail.com>
2025-11-08 00:49:32 +0100

    fixed build errors

--- app/buy/page.tsx
+++ app/buy/page.tsx
@@ -1,14 +1,21 @@
 'use client'
 import { BuyPage } from '@/components/BuyPage'
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
-export default function Buy() {
+function BuyContent() {
   const searchParams = useSearchParams()
   const query = searchParams.get('query') || ''
 
+  return <BuyPage query={query} />
+}
+
+export default function Buy() {
   return (
     <div className='min-h-screen flex flex-col'>
-      <BuyPage query={query} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <BuyContent />
+      </Suspense>
     </div>
   )
 }
--- app/rent/page.tsx
+++ app/rent/page.tsx
@@ -2,14 +2,21 @@
 
 import { RentPage } from '@/components/RentPage'
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
-export default function Rent() {
+function RentContent() {
   const searchParams = useSearchParams()
   const query = searchParams.get('query') || ''
 
+  return <RentPage query={query} />
+}
+
+export default function Rent() {
   return (
     <div className='min-h-screen flex flex-col'>
-      <RentPage query={query} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <RentContent />
+      </Suspense>
     </div>
   )
 }
